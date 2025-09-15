1d88d53cb39ced08ed418ea4baf45029bb443e16
boreacene <github@boreacene.works>
2025-09-15 00:42:15 -0400

    added player ship, asteroids, movement logic for ship and asteroids

--- main.py
+++ main.py
@@ -1,20 +1,48 @@
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
     pygame.init()
     print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fpsclock = pygame.time.Clock()
+
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+
+    Player.containers = (updatables, drawables)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatables, drawables)
+    AsteroidField.containers = updatables
+
+    asteroidfield = AsteroidField()
+
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0,0,0), rect=None)
+
+        updatables.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawables:
+            obj.draw(screen)
+
         pygame.display.flip()
-        
+
+        # limit the framerate to 60 FPS
+        dt = fpsclock.tick(60) / 1000
 
 
 if __name__ == "__main__":
